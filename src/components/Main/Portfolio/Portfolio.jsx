9e3a3172feced57c8147323a93c742117a636a72
MarcusFrei <masterzero2012@yandex.ru>
2024-02-09 17:32:54 +0300

    small fixes

--- src/components/Main/Portfolio/Portfolio.jsx
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -7,15 +7,24 @@ const Portfolio = (props) => {
     <section className="portfolio main-section">
       <h5 className="portfolio__title">Портфолио</h5>
       <div className="portfolio__list">
-        <a className="portfolio__list_item" href="#">
+        <a
+          className="portfolio__list_item"
+          href="https://github.com/MarcusFrei/how-to-learn"
+        >
           <span className="portfolio__link">Статичный сайт</span>
           <img src={arrow} alt="arrow" />
         </a>
-        <a className="portfolio__list_item" href="#">
+        <a
+          className="portfolio__list_item"
+          href="https://github.com/MarcusFrei/russian-travel"
+        >
           <span className="portfolio__link">Адаптивный сайт</span>
           <img src={arrow} alt="arrow" />
         </a>
-        <a className="portfolio__list_item" href="#">
+        <a
+          className="portfolio__list_item"
+          href="https://github.com/MarcusFrei/mesto"
+        >
           <span className="portfolio__link">Одностраничное приложение</span>
           <img src={arrow} alt="arrow" />
         </a>
